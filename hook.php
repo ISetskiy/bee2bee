5f00e0ff12c10732c507f56bca4126047fb1f063
ISetskiy <Chasovoy57@mail.ru>
2015-09-22 23:09:57 +0300

    Update hook.php

--- hook.php
+++ hook.php
@@ -6,7 +6,7 @@ $API_KEY = '128600740:AAHZzj8zRHIZx333bPuSMn_HmCQGcnghStU';
 $BOT_NAME = 'Beeline_b2b_spb_bot';
 
 try {
-    $telegram = new isetskiy\bee2bee\Telegram($API_KEY,$BOT_NAME);
+    $telegram = new isetskiy\bee2bee ($API_KEY,$BOT_NAME);
 
     $telegram->handle();
 } catch (isetskiy\bee2bee\Exception\TelegramException $e) {
